2b3d479eedeb052573ee06dff06f86fc5f7e6bb1
ryotaishigaki <ishigakiryota@occ.co.jp>
2021-06-02 16:36:38 +0900

    modelファイルにDivision.cs(課のテーブルプロパティを作成)

---
+++ Models/Division.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThanksCardAPI.Models
+{
+    public class Division
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long? DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
